2ca93105c52d7b0e2f55082e53c917abafb8d9dc
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-08-30 02:44:50 +0200

    Under construction page

--- UI/Web/src/app/management/management-server/management-server.component.ts
+++ UI/Web/src/app/management/management-server/management-server.component.ts
@@ -1,8 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {UnderConstructionComponent} from '../../shared/components/under-construction/under-construction.component';
 
 @Component({
   selector: 'app-management-server',
-  imports: [],
+  imports: [
+    UnderConstructionComponent
+  ],
   templateUrl: './management-server.component.html',
   styleUrl: './management-server.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
